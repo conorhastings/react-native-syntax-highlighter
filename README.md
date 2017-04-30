89586108f59d33e6fa50543b5b9f268286334f03
Conor Hastings <hastings.conorm@gmail.com>
2017-04-30 16:30:43 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,25 @@
+## React Native Syntax Highlighter
+
+
+Syntax highlighting component for `React Native` using <a href='https://github.com/conorhastings/react-syntax-highlighter'> `react-syntax-highlighter`</a>
+
+### Install
+
+`npm install react-native-syntax-highlighter --save`
+
+
+### Use
+
+#### props
+Accepts all of the same props as <a href='https://github.com/conorhastings/react-syntax-highlighter'> `react-syntax-highlighter`</a> with two additional props.
+* `fontFamily` - the font family to use for syntax text.
+* `fontSize` - the fontSize for syntax text.
+
+```js
+import SyntaxHighlighter from 'react-native-syntax-highlighter';
+import { docco } from 'react-syntax-highlighter/dist/styles';
+const Component = () => {
+  const codeString = '(num) => num + 1';
+  return <SyntaxHighlighter language='javascript' style={docco}>{codeString}</SyntaxHighlighter>;  
+}
+```
---
+++ src/index.js
@@ -0,0 +1,113 @@
+import React from 'react';
+import { Text, View, ScrollView } from 'react-native';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { createStyleObject } from 'react-syntax-highlighter/dist/create-element';
+import { defaultStyle } from'react-syntax-highlighter/dist/styles';
+
+function generateNewStylesheet(stylesheet) {
+  return Object.entries(stylesheet).reduce((newStylesheet, [className, style]) => {
+    newStylesheet[className] = Object.entries(style).reduce((newStyle, [key, value]) => {
+      if (key === 'overflowX') {
+        newStyle.overflow = value === 'auto' ? 'scroll' : value;
+      }
+      else if (value.includes('em')) {
+        const [num] = value.split('em');
+        newStyle[key] = Number(num) * 1;
+      }
+      else if (key === 'background') {
+        newStyle.backgroundColor = value;
+      }
+      else {
+        newStyle[key] = value;
+      }
+      return newStyle;
+    }, {});
+    return newStylesheet;
+  }, {});
+}
+
+function createChildren(stylesheet) {
+  let childrenCount = 0;
+  return (children, defaultColor) => {
+    childrenCount += 1;
+    return children.map((child, i) => createNativeElement({
+      node: child,
+      stylesheet,
+      key:`code-segment-${childrenCount}-${i}`,
+      defaultColor
+    }));
+  }
+}
+
+function createNativeElement({ node, stylesheet, key, defaultColor, fontFamily, fontSize = 12 }) {
+  const { properties, type, tagName: TagName, value } = node;
+  if (type === 'text') {
+    return (
+      <Text 
+        key={key} 
+        style={{ color: defaultColor, fontFamily, fontSize, height: fontSize + 2}}
+      >
+        {value}
+      </Text>
+    );
+  } else if (TagName) {
+    const childrenCreator = createChildren(stylesheet);
+    const style = createStyleObject(
+      properties.className,
+      Object.assign(
+        { color: defaultColor }, 
+        properties.style, 
+        { fontFamily, fontSize, height: fontSize + 2 }
+      ),
+      stylesheet
+    );
+    const children = childrenCreator(node.children, style.color || defaultColor);
+    return <Text key={key} style={style}>{children}</Text>;
+  }
+}
+
+function nativeRenderer({ defaultColor, fontFamily, fontSize }) {
+  return ({ rows, stylesheet }) => {
+    return rows.map((node, i) => createNativeElement({
+      node,
+      stylesheet,
+      key: `code-segment-${i}`,
+      defaultColor,
+      fontFamily,
+      fontSize
+    }))
+  }
+}
+
+
+function NativeSyntaxHighlighter({ fontFamily, fontSize, style, children, ...rest}) {
+  const transformedStyle = generateNewStylesheet(style);
+  const defaultColor = transformedStyle.hljs && transformedStyle.hljs.color || '#000';
+  if (transformedStyle.hljs && transformedStyle.hljs.color) {
+    delete transformedStyle.hljs.color;
+  }
+  return (
+    <SyntaxHighlighter
+      {...rest}
+      style={transformedStyle}
+      horizontal={true}
+      renderer={(nativeRenderer({ 
+        defaultColor, 
+        fontFamily,
+        fontSize
+      }))}
+    >
+      {children}
+    </SyntaxHighlighter>
+  );
+}
+
+NativeSyntaxHighlighter.defaultProps = {
+  fontFamily: 'Menlo-Regular',
+  fontSize: 12,
+  style: defaultStyle,
+  PreTag: ScrollView,
+  CodeTag: ScrollView
+};
+
+export default NativeSyntaxHighlighter;
